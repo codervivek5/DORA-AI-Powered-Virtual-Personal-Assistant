0265a12781ac9742afb8c3f9b907d74ffc26465a
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-08 13:41:04 +0530

    Added frontend using next js and added shadcn component library

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "DORA AI - Your AI-Powered Virtual Personal Assistant",
+  description: "DORA AI is an intelligent virtual personal assistant designed to streamline your daily tasks, manage your schedule, and help you stay organized effortlessly.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
